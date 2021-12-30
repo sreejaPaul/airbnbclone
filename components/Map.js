440c71f3324ee7fee0a72702d0dd6ad3cf5be929
sreejaPaul <sreeja1998paul@gmail.com>
2021-12-30 22:27:18 +0530

    Mapbox Integration Done

--- components/Map.js
+++ components/Map.js
@@ -1,7 +1,7 @@
 import React , {useState} from 'react';
 import ReactMapGL, {Marker, Popup} from 'react-map-gl';
 import { getCenter } from 'geolib';
-
+import {LocationMarkerIcon, UserCircleIcon} from '@heroicons/react/solid';
 
 function Map({searchResults}) {
     const [selectedLocation, setSelectedLocation] = useState({});
@@ -32,8 +32,8 @@ function Map({searchResults}) {
                 searchResults.map((res)=>(
                     <div key={res.long}>
                         <Marker longitude={res.long} latitude={res.lat} offsetLeft={-20} offsetTop={-10}>
-                            <p className='cursor-pointer text-2xl animate-bounce' onClick={()=>setSelectedLocation(res)}>
-                                <svg className="w-6 h-6" fill="#FE595E" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
+                            <p className='cursor-pointer text-sm text-red-400 font-bold animate-bounce' onClick={()=>setSelectedLocation(res)}>
+                                <LocationMarkerIcon style={{color:"#FD5B61", height:"30px"}} className='text-sm'/>Hotel
                             </p>
                         </Marker>
                         {selectedLocation.long === res.long ? <Popup closeOnClick={true} onClose={()=>setSelectedLocation({})} latitude ={res.lat} longitude={res.long}> {res.title} </Popup>: (false) }
